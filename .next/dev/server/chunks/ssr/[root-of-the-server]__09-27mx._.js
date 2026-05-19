269a250aa4cee4be09960fc18da2d070916360dd
godtail2010 <godtail2010@gmail.com>
2026-05-19 13:10:40 +0900

    a

--- .next/dev/server/chunks/ssr/[root-of-the-server]__09-27mx._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__09-27mx._.js
@@ -214,8 +214,16 @@ function AccessPage() {
                                                             columnNumber: 19
                                                         }, this),
                                                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
-                                                            children: "神奈川県横浜市旭区鶴ケ峰2-67-6 セイワビル302"
-                                                        }, void 0, false, {
+                                                            children: [
+                                                                "〒241-0022",
+                                                                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("br", {}, void 0, false, {
+                                                                    fileName: "[project]/src/app/access/page.tsx",
+                                                                    lineNumber: 75,
+                                                                    columnNumber: 31
+                                                                }, this),
+                                                                "神奈川県横浜市旭区鶴ケ峰2-67-6 セイワビル302"
+                                                            ]
+                                                        }, void 0, true, {
                                                             fileName: "[project]/src/app/access/page.tsx",
                                                             lineNumber: 75,
                                                             columnNumber: 19
--- .next/dev/server/chunks/ssr/[root-of-the-server]__0teyr9h._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__0teyr9h._.js
@@ -1296,8 +1296,16 @@ function Home() {
                                                             columnNumber: 21
                                                         }, this),
                                                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
-                                                            children: "〒241-0022 神奈川県横浜市旭区鶴ケ峰2-67-6 セイワビル302"
-                                                        }, void 0, false, {
+                                                            children: [
+                                                                "〒241-0022",
+                                                                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("br", {}, void 0, false, {
+                                                                    fileName: "[project]/src/app/page.tsx",
+                                                                    lineNumber: 431,
+                                                                    columnNumber: 33
+                                                                }, this),
+                                                                "神奈川県横浜市旭区鶴ケ峰2-67-6 セイワビル302"
+                                                            ]
+                                                        }, void 0, true, {
                                                             fileName: "[project]/src/app/page.tsx",
                                                             lineNumber: 431,
                                                             columnNumber: 21
